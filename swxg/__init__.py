f6f939939dcae23615e8929c230eed5f85be17e2
Alexander Benjamin Thames <ayt5134@psu.edu>
2025-10-21 21:03:05 -0400

    .pkl files don't need to be seen by documentation

--- swxg/__init__.py
+++ swxg/__init__.py
@@ -1,5 +1,9 @@
+import os
 from .model import SWXGModel
-from . import test_wx
+#from . import test_wx
 
-__all__ = ["SWXGModel", "test_wx"]
-
+#__all__ = ["SWXGModel", "test_wx"]
+__all__ = ["SWXGModel"]
+if not os.getenv("READTHEDOCS"):
+    from . import test_wx
+    __all__.append("test_wx")
